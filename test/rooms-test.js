31c8628e52c8c3866c71f3c4c35eeb04f7105fbb
Maia Cochran <maia.cochran1@gmail.com>
2022-08-03 23:04:12 -0700

    Refactor customer test and customer class file to pass

---
+++ test/rooms-test.js
@@ -0,0 +1,21 @@
+import chai from 'chai';
+import Room from '../src/classes/rooms';
+import roomData from '../src/data/roomData';
+const expect = chai.expect;
+
+describe('Room Info', () => {
+//global variables:
+    let room;
+    let roomData;
+    beforeEach(() => {
+        room = new Customer(roomData)
+    })
+
+    it('should be a function', function() {
+    expect(Room).to.be.a('function');
+    });    
+    it('should be an instance of Room', function() {
+        expect(room).to.be.an.instanceOf(Room);
+
+    });
+});
