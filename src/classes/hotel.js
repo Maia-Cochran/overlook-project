5e61863673dd8efa3cb201e3e35cd74b7b6c1b2d
Maia Cochran <maia.cochran1@gmail.com>
2022-08-07 16:08:19 -0700

    Refactor bookings display and clean up pseudo code

---
+++ src/classes/hotel.js
@@ -0,0 +1,9 @@
+class Hotel {
+    constructor(bookingData, customerData, roomData){
+        this.allBookings = bookingData;
+        this.allCustomers = customerData;
+        this.allRooms = roomData;
+    }
+}
+
+export default Hotel
---
+++ test/hotel-test.js
@@ -0,0 +1,10 @@
+import chai from 'chai';
+import Hotel from '../src/classes/hotel';
+
+const expect = chai.expect;
+
+// describe('See if the tests are running', function() {
+//   it('should return true', function() {
+//     expect(true).to.equal(true);
+//   });
+// });
